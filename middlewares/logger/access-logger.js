11c241886375883281acfc718a98a505e441081f
Cyril Panicker <Cyril.Panicker@walmart.com>
2019-06-28 14:54:37 +0530

    loggers will now add context if not already present

--- middlewares/logger/access-logger.js
+++ middlewares/logger/access-logger.js
@@ -1,13 +1,26 @@
 const morgan = require("morgan");
 const winston = require("winston");
+const uuid = require("node-uuid");
 const getLogger = require("./logger");
 const MESSAGE = Symbol.for("message");
 
-morgan.token("id", (request) => request.context.id);
+morgan.token("id", (request) => {
+  if(!request.context){
+    const id = uuid.v4();
+    request.context = { id };
+    return id;
+  }
+  if(!request.context.id){
+    const id = uuid.v4();
+    request.context.id = id;
+    return id;
+  }
+  return request.context.id;
+});
 morgan.token("date", () => new Date().toISOString());
 
-const requestLogFormat = `:id | :date | REQUEST | :remote-addr | :remote-user | :method :url HTTP/:http-version | :referrer | :user-agent`;
-const responseLogFormat = `:id | :date | RESPONSE | :method :url HTTP/:http-version | :status | :res[content-length]`;
+const requestLogFormat = `:date | :id | REQUEST | :remote-addr | :remote-user | :method :url HTTP/:http-version | :referrer | :user-agent`;
+const responseLogFormat = `:date | :id | RESPONSE | :method :url HTTP/:http-version | :status | :res[content-length]`;
 
 const getFormat = () => winston.format((logEntry) => {
   logEntry[MESSAGE] = `${logEntry.message.trim()}`;
--- middlewares/logger/custom-logger.js
+++ middlewares/logger/custom-logger.js
@@ -1,15 +1,24 @@
 const winston = require("winston");
+const uuid = require("node-uuid");
 const getLogger = require("./logger");
 const MESSAGE = Symbol.for("message");
 
 const getFormat = ({ id }) => winston.format((logEntry, options) => {
   const { level, message } = logEntry;
   const timestamp = new Date().toISOString();
-  logEntry[MESSAGE] = `${options.id} | ${timestamp} | ${level.toUpperCase()} | ${message}`;
+  logEntry[MESSAGE] = `${timestamp} | ${options.id} | ${level.toUpperCase()} | ${message}`;
   return logEntry;
 })({ id });
 
 const customLogger = (request, _, next) => {
+  if(!request.context){
+    const id = uuid.v4();
+    request.context = { id };
+  }
+  if(!request.context.id){
+    const id = uuid.v4();
+    request.context.id = id;
+  }
   request.context.logger = getLogger(getFormat(request.context));
   next();
 };
